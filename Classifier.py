0e61814f16e8bfe37dd2a2fec719a1f36c556cf0
james-k0 <159821339+james-k0@users.noreply.github.com>
2024-05-09 04:14:09 +0100

    Remodelling, faster running, feature adds
    can now save model, load model.
    Model has changed, hyperparameters have changed (I believe), move to transforms v1 due to apparently v2 is more volatile on py 3.12 source i forgot, now runs the cudnn benchmark, everything runs on cuda with non_blocking (which may be bad im not certain as some training may have the requirement of being sequential), now uses pin memory=true and 4 workers for dataloaders, test and val dataloaders dont need to be shuffled, added dropout to the nn for more generality (regularisation), added function wise training and evaluation, fixed training printing every batch,  the to py script is pretty cool too.

--- Classifier.py
+++ Classifier.py
@@ -3,7 +3,7 @@
 
 # # Lets import some things
 
-# In[25]:
+# In[1]:
 
 
 import numpy as np
@@ -14,30 +14,31 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 import torchvision
 from torchvision import datasets
-import torchvision.transforms.v2 as transforms
+import torchvision.transforms as transforms
 
 
 # # Decide if cuda
 
-# In[26]:
+# In[2]:
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
+torch.backends.cudnn.benchmark = True
 
 
 # # Load dataset
 
-# In[27]:
+# In[3]:
 
 
-batchsize = 16
+batchsize = 64
 num_classes = 102
-learning_rate = 0.001
+learning_rate = 0.0015
 num_epochs = 50
 
 
-# In[28]:
+# In[4]:
 
 
 trainingData = datasets.Flowers102(
@@ -48,7 +49,8 @@ trainingData = datasets.Flowers102(
         transforms.Resize((256, 256)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(10),
-        transforms.ToTensor()
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor(),
     ])
 )
 testData = datasets.Flowers102(
@@ -73,17 +75,17 @@ valData = datasets.Flowers102(
 
 # # Get some dataloaders
 
-# In[29]:
+# In[5]:
 
 
-train_dataloader = DataLoader(trainingData, batch_size=batchsize, shuffle=True, num_workers=4)
-test_dataloader = DataLoader(testData, batch_size=batchsize, shuffle=True,num_workers=4)
-val_dataloader = DataLoader(valData, batch_size=batchsize, shuffle=True,num_workers=4)
+train_dataloader = DataLoader(trainingData, batch_size=batchsize, shuffle=True, num_workers=4, pin_memory=True)
+test_dataloader = DataLoader(testData, batch_size=batchsize, shuffle=False, num_workers=4, pin_memory=True)
+val_dataloader = DataLoader(valData, batch_size=batchsize, shuffle=False, num_workers=4, pin_memory=True)
 
 
 # # Neural Network class
 
-# In[30]:
+# In[6]:
 
 
 class NeuralNet(nn.Module):
@@ -92,14 +94,15 @@ class NeuralNet(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(3,32,kernel_size=5,stride=1,padding=2),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),#half size
             nn.Conv2d(32,64,kernel_size=5,stride=1,padding=2),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2),#half size
         )
         self.classifier = nn.Sequential(
             nn.Linear(64*64*64 ,1024),
             nn.ReLU(),
+            nn.Dropout(0.2), #add a bit of randomness for some fun  + generality
             nn.Linear(1024,102),
         )
         
@@ -109,82 +112,96 @@ class NeuralNet(nn.Module):
         x= x.view(x.size(0),-1)
         x = self.classifier(x)
         return x
-#conv way selected, this is a uhhh LeNet5 for MNIST so it doesnt work like yeah of course pretty placeholder
+# i should stop drinking because where did i imagine this up !?
 
-
-# # todo possibly do the image display thing/ save&load models/ https://pytorch.org/tutorials/beginner/introyt/introyt1_tutorial.html / tune hyperparams
 
 # # Model = something
 
-# In[31]:
+# In[7]:
 
 
-model = NeuralNet().to(device)
+model = NeuralNet().to(device,non_blocking=True)
 cost = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 
 # # Actually do the training, needs to print less often
 
-# In[32]:
+# In[ ]:
 
 
-for epoch in range(num_epochs):
-    for i, (images,labels) in enumerate(train_dataloader):
-        images = images.to(device)
-        labels = labels.to(device)
-        
-        outputs = model(images)
-        loss = cost(outputs, labels)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        print(f'Epoch: [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+def train():
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        batches = 0
+        for i, (images,labels) in enumerate(train_dataloader):
+            images = images.to(device, non_blocking=True)
+            labels = labels.to(device, non_blocking=True)
+            outputs = model(images)
+            optimizer.zero_grad()
+            loss = cost(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            batches +=1
+        print(f'Epoch: {epoch+1}, Avg Loss: {running_loss/batches:4f}, Num Batches: {batches}')
+train()
+# next steps: the running loss is a bit jank since batches is always 16 it will print the same batch, should be shuffled though but doesnt look like it ? 
+# maybe im confusing shuffles with individual weights
 
 
 # # Display the training, testing, validation accuracy
 
-# In[34]:
+# In[ ]:
 
 
-with torch.no_grad():
+def evaluate(dataloader):
+    model.eval()
     correct = 0
-    total = 0
-    for images,labels in train_dataloader:
-        images,labels = images.to(device),labels.to(device)
-        outputs = model(images)
-        _,predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted==labels).sum().item()
-    acc = 100 * correct/total
-print(f'Accuracy on train data: {acc}')
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images,labels in test_dataloader:
-        images,labels = images.to(device),labels.to(device)
-        outputs = model(images)
-        _,predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted==labels).sum().item()
-    acc = 100 * correct/total
-print(f'Accuracy on test data: {acc}')
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images,labels in val_dataloader:
-        images,labels = images.to(device),labels.to(device)
-        outputs = model(images)
-        _,predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted==labels).sum().item()
-    acc = 100 * correct/total
-print(f'Accuracy on val data: {acc}')
+    total =0
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted==labels).sum().item()
+        acc= float(100*correct/total)
+    return acc
+print(f'val acc: {evaluate(val_dataloader):.3f}%')
+print(f'test acc: {evaluate(test_dataloader):.3f}%')
+print(f'train acc: {evaluate(train_dataloader):.3f}%')
 
+
+# # Save model
+
+# In[ ]:
+
+
+def save(pathname):
+    torch.save(NeuralNet().state_dict(), f'{pathname}.pth')
+    print(f'Saved model to {pathname}.pth')
+
+
+# # Load model
+
+# In[ ]:
+
+
+def load(pathname):
+    model = NeuralNet().to(device)
+    model.load_state_dict(torch.load(f'{pathname}.pth'))
+    print(f'Loaded model from {pathname}.pth')
+
+
+# # Command line to convert this notebook to a python file, the reason is for readability of the code from github lol
 
 # In[ ]:
 
 
 get_ipython().system('jupyter nbconvert --to script Classifier.ipynb')
+
+
+# # todo possibly do the image display thing/ https://pytorch.org/tutorials/beginner/introyt/introyt1_tutorial.html / tune hyperparams
