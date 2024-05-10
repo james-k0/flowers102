3da4b5ba6b0e6de0580e7e4ef154f68ec478cac5
james-k0 <159821339+james-k0@users.noreply.github.com>
2024-05-10 11:36:03 +0100

    Progris riport
    trying different loss functions and added a scheduler and training returns val acc array just because

--- Classifier.py
+++ Classifier.py
@@ -3,7 +3,7 @@
 
 # # Lets import some things
 
-# In[ ]:
+# In[1]:
 
 
 import numpy as np
@@ -20,7 +20,7 @@ import time
 
 # # Decide if cuda
 
-# In[ ]:
+# In[2]:
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -30,16 +30,16 @@ torch.backends.cudnn.benchmark = True
 
 # # Load dataset
 
-# In[ ]:
+# In[3]:
 
 
-batchsize = 32
+batchsize = 16
 num_classes = 102
-learning_rate = 0.0001
-num_epochs = 2  0
+learning_rate = 0.01
+num_epochs = 20
 
 
-# In[ ]:
+# In[4]:
 
 
 trainingData = datasets.Flowers102(
@@ -51,7 +51,7 @@ trainingData = datasets.Flowers102(
         # transforms.RandomResizedCrop((256, 256),antialias=True),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(30), 
-        transforms.ColorJitter(contrast=0.1,hue=0.1,brightness=0.1, saturation=0.1),
+        transforms.ColorJitter(contrast=0.2,hue=0.2,brightness=0.2, saturation=0.2),
         transforms.RandomAffine(degrees=0,translate=(0.1,0.1)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -77,7 +77,7 @@ valData = datasets.Flowers102(
 )
 
 
-# In[ ]:
+# In[5]:
 
 
 print(f'training data has: {len(trainingData)} images')
@@ -87,42 +87,42 @@ print(f'test data has: {len(testData)} images')
 
 # # Get some dataloaders
 
-# In[ ]:
+# In[6]:
 
 
-train_dataloader = DataLoader(trainingData, batch_size=batchsize, shuffle=True, num_workers=12, pin_memory=True, persistent_workers=True,prefetch_factor=6)
+train_dataloader = DataLoader(trainingData, batch_size=batchsize, shuffle=True, num_workers=8, pin_memory=True, persistent_workers=True,prefetch_factor=2)
 test_dataloader = DataLoader(testData, batch_size=batchsize, shuffle=False, num_workers=4, pin_memory=True)
 val_dataloader = DataLoader(valData, batch_size=batchsize, shuffle=False, num_workers=4, pin_memory=True)
 
 
 # # Neural Network class
 
-# In[ ]:
+# In[7]:
 
 
 class NeuralNet(nn.Module):
     def __init__(self):
         super(NeuralNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3,32,kernel_size=3,padding=1,bias=False),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),#half size stride defaults to kernel size
-            nn.Conv2d(32,64,kernel_size=3,padding=1,bias=False),
+            nn.Conv2d(3,64,kernel_size=3,padding=1,bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=2),#half size stride defaults to kernel size
             nn.Conv2d(64,128,kernel_size=3,padding=1,bias=False),
             nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(128,256,kernel_size=3,padding=1,bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2)
         )
         self.classifier = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(128*32*32,1024),
+            nn.Linear(256*32*32,2048),
             nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(1024,102),
+            nn.Linear(2048,102),
             nn.LogSoftmax(dim=1)
         )
         
@@ -136,21 +136,23 @@ class NeuralNet(nn.Module):
 
 # # Model = something
 
-# In[ ]:
+# In[8]:
 
 
 model = NeuralNet().to(device, non_blocking=True)
-cost = nn.CrossEntropyLoss()
+cost = nn.NLLLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=30,gamma=0.1)
 
 
 # # Actually do the training, needs to print less often
 
-# In[ ]:
+# In[9]:
 
 
 def train():
     loss_per_epoch = [] # clear all data incase retraining
+    val_acc_per = []
     for epoch in range(num_epochs):
         epoch_start = time.time()
         running_loss = 0.0
@@ -164,22 +166,25 @@ def train():
             loss.backward()
             optimizer.step()
             
-            running_loss += loss.item()
+            
+            running_loss += loss.item() #so this line is a sync point for the cpu and gpu commenting it out greatly reduces the training time (up to 25%  in my testing)
             batches +=1
+        scheduler.step()
         loss_per_epoch.append(running_loss / batches)
         epoch_length = time.time() - epoch_start
         val_acc = evaluate(val_dataloader)
+        val_acc_per.append(val_acc)
         print(f'Epoch: {epoch+1}, Avg Loss: {running_loss/batches:4f}, Num Batches: {batches}, Epoch Time: {epoch_length:.2f}, Val Acc: {val_acc:.3f}%')
-    return loss_per_epoch
+    return loss_per_epoch, val_acc_per
 
 
 # # Plot the avg loss against epochs
 
-# In[ ]:
+# In[10]:
 
 
-def plot_train(epoch_losses):
-    plt.plot(epoch_losses, label='Training Loss')
+def plot_array(array):
+    plt.plot(array, label=f'{array.__name__}')
     # plt.plot(validation_epoch_losses,label='Validation Loss')   
     plt.legend()
     plt.show()
@@ -187,7 +192,7 @@ def plot_train(epoch_losses):
 
 # # Display the training, testing, validation accuracy
 
-# In[ ]:
+# In[11]:
 
 
 def evaluate(dataloader):
@@ -204,12 +209,9 @@ def evaluate(dataloader):
             correct += (predicted==labels).sum().item()
         acc= float(100*correct/total)
     return acc
-# print(f'val acc: {evaluate(val_dataloader):.3f}%')
-# print(f'test acc: {evaluate(test_dataloader):.3f}%')
-# print(f'train acc: {evaluate(train_dataloader):.3f}%')
 
 
-# In[ ]:
+# In[12]:
 
 
 def all_eval():
@@ -220,7 +222,7 @@ def all_eval():
 
 # # Save model
 
-# In[ ]:
+# In[13]:
 
 
 def save(pathname):
@@ -230,7 +232,7 @@ def save(pathname):
 
 # # Load model
 
-# In[ ]:
+# In[14]:
 
 
 def load(pathname):
@@ -239,32 +241,35 @@ def load(pathname):
     print(f'Loaded model from {pathname}.pth')
 
 
-# In[ ]:
+# # 
+
+# In[15]:
 
 
 # def main():
 #     pass
 
 
-# In[ ]:
+# In[16]:
 
 
 if __name__ == '__main__':
-    training_epoch_losses = train()
-    plot_train(training_epoch_losses)
+    training_epoch_losses, val_acc_per = train()
+    plot_array(training_epoch_losses)
+    plot_array(val_acc_per)
     all_eval()
 
 
 # # Command line to convert this notebook to a python file, the reason is for readability of the code from github lol
 
-# In[1]:
+# In[17]:
 
 
 def convert():
     get_ipython().system('jupyter nbconvert --to script Classifier.ipynb')
 
 
-# In[ ]:
+# In[18]:
 
 
 convert()
@@ -272,7 +277,7 @@ convert()
 
 # # todo possibly do the image display thing/ https://pytorch.org/tutorials/beginner/introyt/introyt1_tutorial.html / tune hyperparams
 
-# In[ ]:
+# In[18]:
 
 
 
