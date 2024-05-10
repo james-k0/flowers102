8e6b27f96445015af80268f9f120e6bc7cdeb999
james-k0 <159821339+james-k0@users.noreply.github.com>
2024-05-10 08:10:15 +0100

    Model tuning + modularisation
    If I am correct I could improve the generalisation by lowering the batch size and making the learning rate smaller.
    
    also made some things modular because i believe it should run faster if I am using the __name__ == __main__ properly.

--- Classifier.py
+++ Classifier.py
@@ -3,7 +3,7 @@
 
 # # Lets import some things
 
-# In[1]:
+# In[16]:
 
 
 import numpy as np
@@ -19,7 +19,7 @@ import torchvision.transforms.v2 as transforms
 
 # # Decide if cuda
 
-# In[2]:
+# In[17]:
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -29,16 +29,16 @@ torch.backends.cudnn.benchmark = True
 
 # # Load dataset
 
-# In[3]:
+# In[18]:
 
 
-batchsize = 48
+batchsize = 16
 num_classes = 102
-learning_rate = 0.0015
+learning_rate = 0.001
 num_epochs = 100
 
 
-# In[4]:
+# In[19]:
 
 
 trainingData = datasets.Flowers102(
@@ -48,10 +48,11 @@ trainingData = datasets.Flowers102(
     transform = transforms.Compose([
         transforms.Resize((256,256)),
         # transforms.RandomResizedCrop((256, 256),antialias=True),
-        transforms.RandomRotation(30), 
         transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(30), 
+        transforms.ColorJitter(contrast=0.1,hue=0.1,brightness=0.1, saturation=0.1),
         transforms.ToTensor(),
-        # transforms.Normalize([0.5,],[0.2,])
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 )
 testData = datasets.Flowers102(
@@ -74,7 +75,7 @@ valData = datasets.Flowers102(
 )
 
 
-# In[5]:
+# In[20]:
 
 
 print(f'training data has: {len(trainingData)} images')
@@ -84,39 +85,43 @@ print(f'test data has: {len(testData)} images')
 
 # # Get some dataloaders
 
-# In[6]:
+# In[21]:
 
 
-train_dataloader = DataLoader(trainingData, batch_size=batchsize, shuffle=True, num_workers=4, pin_memory=True)
+train_dataloader = DataLoader(trainingData, batch_size=batchsize, shuffle=True, num_workers=4, pin_memory=True, persistent_workers=True, prefetch_factor=6)
 test_dataloader = DataLoader(testData, batch_size=batchsize, shuffle=False, num_workers=4, pin_memory=True)
 val_dataloader = DataLoader(valData, batch_size=batchsize, shuffle=False, num_workers=4, pin_memory=True)
 
 
 # # Neural Network class
 
-# In[7]:
+# In[22]:
 
 
 class NeuralNet(nn.Module):
     def __init__(self):
         super(NeuralNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3,32,kernel_size=5,stride=1,padding=2),
+            nn.Conv2d(3,32,kernel_size=3,padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),#half size
-            nn.Conv2d(32,64,kernel_size=5,stride=1,padding=2),
+            nn.MaxPool2d(kernel_size=2),#half size stride defaults to kernel size
+            nn.Conv2d(32,64,kernel_size=3,padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),#half size
-            nn.Conv2d(64,128,kernel_size=5,stride=1,padding=2),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(64,128,kernel_size=3,padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2)
         )
         self.classifier = nn.Sequential(
-            nn.Linear(64*64*32 ,1024),
+            nn.Dropout(0.5),
+            nn.Linear(128*32*32,1024),
             nn.ReLU(),
-            nn.Dropout(0.5), #add a bit of randomness for some fun  + generality
+            nn.Dropout(0.5),
             nn.Linear(1024,102),
-            nn.ReLU()
+            nn.LogSoftmax(dim=1)
         )
         
         
@@ -129,29 +134,27 @@ class NeuralNet(nn.Module):
 
 # # Model = something
 
-# In[8]:
+# In[23]:
 
 
-model = NeuralNet().to(device,non_blocking=True)
+model = NeuralNet().to(device, non_blocking=True)
 cost = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 
 # # Actually do the training, needs to print less often
 
-# In[ ]:
+# In[24]:
 
 
-training_epoch_losses = []
 def train():
+    loss_per_epoch = [] # clear all data incase retraining
     for epoch in range(num_epochs):
         running_loss = 0.0
         batches = 0
         for i, (images,labels) in enumerate(train_dataloader):
             images = images.to(device, non_blocking=True)
             labels = labels.to(device, non_blocking=True)
-            # images = images.to(device)
-            # labels = labels.to(device)
             outputs = model(images)
             optimizer.zero_grad()
             loss = cost(outputs, labels)
@@ -160,25 +163,26 @@ def train():
             
             running_loss += loss.item()
             batches +=1
-        training_epoch_losses.append(running_loss/batches)
+        loss_per_epoch.append(running_loss / batches)
         print(f'Epoch: {epoch+1}, Avg Loss: {running_loss/batches:4f}, Num Batches: {batches}')
-train()
+    return loss_per_epoch
 
 
 # # Plot the avg loss against epochs
 
-# In[ ]:
+# In[25]:
 
 
-plt.plot(training_epoch_losses, label='Training Loss')
-# plt.plot(validation_epoch_losses,label='Validation Loss')   
-plt.legend()
-plt.show()
+def plot_train(epoch_losses):
+    plt.plot(epoch_losses, label='Training Loss')
+    # plt.plot(validation_epoch_losses,label='Validation Loss')   
+    plt.legend()
+    plt.show()
 
 
 # # Display the training, testing, validation accuracy
 
-# In[ ]:
+# In[26]:
 
 
 def evaluate(dataloader):
@@ -195,14 +199,23 @@ def evaluate(dataloader):
             correct += (predicted==labels).sum().item()
         acc= float(100*correct/total)
     return acc
-print(f'val acc: {evaluate(val_dataloader):.3f}%')
-print(f'test acc: {evaluate(test_dataloader):.3f}%')
-print(f'train acc: {evaluate(train_dataloader):.3f}%')
+# print(f'val acc: {evaluate(val_dataloader):.3f}%')
+# print(f'test acc: {evaluate(test_dataloader):.3f}%')
+# print(f'train acc: {evaluate(train_dataloader):.3f}%')
+
+
+# In[27]:
+
+
+def all_eval():
+    print(f'val acc: {evaluate(val_dataloader):.3f}%')
+    print(f'test acc: {evaluate(test_dataloader):.3f}%')
+    print(f'train acc: {evaluate(train_dataloader):.3f}%')
 
 
 # # Save model
 
-# In[ ]:
+# In[28]:
 
 
 def save(pathname):
@@ -212,13 +225,29 @@ def save(pathname):
 
 # # Load model
 
-# In[ ]:
+# In[29]:
 
 
 def load(pathname):
-    model = NeuralNet().to(device)
+    # model = NeuralNet().to(device)
     model.load_state_dict(torch.load(f'{pathname}.pth'))
     print(f'Loaded model from {pathname}.pth')
+
+
+# In[30]:
+
+
+# def main():
+#     pass
+
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    training_epoch_losses = train()
+    plot_train(training_epoch_losses)
+    all_eval()
 
 
 # # Command line to convert this notebook to a python file, the reason is for readability of the code from github lol
